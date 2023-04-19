ab719f827d93655f7a5ac6c09998cac59051b904
eeun95 <eeun95@gmail.com>
2023-04-19 23:49:59 +0900

    데이터베이스 연결

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -15,6 +15,12 @@ public class DBConnectionUtil {
             // DB를 연결하기 위해 드라이브 매니저를 사용
             // 커넥션 인터페이스의 구현체인 h2 커넥션을 반환
             Connection connection = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+            /*
+            * DriverManager 가 라이브러리(초기 세팅시 build.gradle에 넣어준 h2 라이브러리)에 등록된 드라이버 목록을 자동으로 인식
+            * 이 드라이버들에게 순서대로 URL, 이름, 비밀번호 등 접속에 필요한 추가 정보를 넘겨 커넥션을 획득할 수 있는지 확인
+            *   - 연결 가능하면 커넥션을 획득, 안되면 다음 드라이버에게 순서 넘김
+            * 찾은 커넥션 구현체를 클라이언트에 반환
+            * */
             log.info("get connection={}, class={}", connection, connection.getClass());
             return connection;
 
