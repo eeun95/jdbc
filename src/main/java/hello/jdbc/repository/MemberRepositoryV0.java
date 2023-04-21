f4264322abb2a2bb8adb1ee04c96a8d157552a5e
eeun95 <eeun95@gmail.com>
2023-04-21 23:25:04 +0900

    JDBC 개발

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -6,6 +6,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -35,8 +36,38 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+    }
+
+    // 리소스 정리는 꼭 해주어야 한다.
+    // 예외가 발생하든 하지 않든 항상 수행되어야 하므로 finally 구문에 주의해서 작성해야 한다.
+    // 이 부분을 놓치면 커넥션이 끊어지지 않고 계속 유지되는 문제가 발생할 수 있다. = 리소스 누수, 커넥션 부족으로 장애발생할 수 있음
     private void close(Connection con, Statement stmt, ResultSet rs) {
-        // pstmt는 stmt에 파라미터를 바인딩하는 기능이 추가된 것
+        // pstmt는 stmt에 파라미터를 바인딩하는 기능이 추가된 것 (SQL Injection 공격 예방)
 
         if(stmt != null) {
             try {
@@ -60,6 +91,7 @@ public class MemberRepositoryV0 {
             }
         }
     }
+
     private Connection getConnection() {
         return DBConnectionUtil.getConnection();
     }
